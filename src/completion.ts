58afbe986286e0e5333709e2b66d08813803611f
Luc Boissaye <luc@boissaye.fr>
2025-06-07 07:46:07 +0100

    feat: improve completion

--- src/completion.ts
+++ src/completion.ts
@@ -1,13 +1,49 @@
-import {completeFromList} from "@codemirror/autocomplete"
+import {completeFromList, snippet, type Completion as CompletionType } from "@codemirror/autocomplete"
 import { formulaLanguage } from './language';
 
+export const completionList: CompletionType[]= [
+  {
+    label: 'if',
+    apply: snippet(`IF(\${true}, \${"ok"}, \${"ko"})`),
+  },
+  {
+    label: 'ISBLANK',
+    apply: snippet(`ISBLANK(\${value})`),
+  },
+  {
+    label: 'NOT',
+    apply: snippet(`NOT(\${value})`),
+  },
+  {
+    label: 'BLANKVALUE',
+    apply: snippet(`BLANKVALUE(\${value1}, \${value2})`),
+  },
+  {
+    label: 'TEXT',
+    apply: snippet(`TEXT(\${12})`),
+  },
+  {
+    label: 'FLOOR',
+    apply: snippet(`FLOOR(\${12.12})`),
+  },
+  {
+    label: 'CEILING',
+    apply: snippet(`CEILING(\${12.12})`),
+  },
+  {
+    label: 'LEN',
+    apply: snippet(`LEN(\${"Text"})`),
+  },
+  {
+    label: 'true',
+    apply: `true`,
+  },
+  {
+    label: 'false',
+    apply: `false`,
+  },
+
+]
 export const Completion = formulaLanguage.data.of({
-  autocomplete: completeFromList([
-    {label: "IF", type: "keyword"},
-    {label: "AND", type: "keyword"},
-    {label: "OR", type: "keyword"},
-    {label: "NOT", type: "function"},
-    {label: "TRUE", type: "function"},
-    {label: "FALSE", type: "function"}
-  ])
+  autocomplete: completeFromList(completionList)
 })
