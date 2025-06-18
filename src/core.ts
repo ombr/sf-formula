0419062702f7a7e44f1e0b263c5003b57e6238ba
Luc Boissaye <luc@boissaye.fr>
2025-06-18 18:54:34 +0200

    feat: typescript import and peerdependencies

---
+++ src/core.ts
@@ -0,0 +1,16 @@
+import { evaluate } from "./evaluate";
+import { defaultFunctions } from "./functions";
+import { parser } from "./parser";
+
+export type Context = Record<string, unknown> | ((variables: string[]) => unknown);
+export type Functions = Record<string, (...args: Array<()=> unknown>)=> unknown>;
+export type Options = {functions: Functions};
+export class FormulaError extends Error {};
+
+export function formulaEval(formula: string, context: Context = {}, options: Options = { functions: defaultFunctions}): unknown {
+  if (typeof formula !== 'string') throw new FormulaError('Formula should be a string');
+  const res = parser.parse(formula);
+  return evaluate(res, formula, context, options);
+}
+
+export { parser, evaluate, defaultFunctions }; 
